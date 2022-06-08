3287b52de07ccfd0d4998e6dd5aeae19227bed95
aliyu <1324149139@qq.com>
2022-06-08 14:26:58 +0800

    可以爬取页面

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
-console.log("hi aliyu");
+const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
+
+let job = '前端'
+let company = '腾讯'
+
+async function start() {
+    let url = `https://www.nowcoder.com/search?type=post&order=recall&query=${company}+${job}&subType=2&tagId=&page=2`
+    let res = await fetch(url);
+    let body = await res.text();
+    console.log(body);
+
+    fs.writeFileSync(path.join(process.cwd(), `${job}_${company}_1.html`), body, 'utf-8')
+}
+
+start()
